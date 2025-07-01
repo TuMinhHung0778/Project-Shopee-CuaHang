5d183ac6f2e829ac150e84fe54662c8624fc7aa5
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-07-01 12:17:47 +0700

    169. Setup Protected Route va Rejected Route trong React Router

--- Shopee/ShopeeClone/src/useRouteElements.tsx
+++ Shopee/ShopeeClone/src/useRouteElements.tsx
@@ -1,36 +1,72 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
+import Profile from './pages/Profile'
+
+const isAuthenticated = true // Giả sử người dùng đã đăng nhập, bạn có thể thay đổi giá trị này dựa trên trạng thái xác thực thực tế của người dùng
+// const isAuthenticated = false // Giả sử người dùng chưa đăng nhập, bạn có thể thay đổi giá trị này dựa trên trạng thái xác thực thực tế của người dùng
+function ProtectedRoute() {
+  // Route này sẽ được sử dụng cho các route yêu cầu xác thực
+  // Ví dụ: trang chủ, trang sản phẩm, trang giỏ hàng, v.v
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  // Route này sẽ được sử dụng cho các route không yêu cầu xác thực
+  // Ví dụ: trang đăng nhập và đăng ký
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <>
+              <RegisterLayout>
+                <Login />
+              </RegisterLayout>
+            </>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
-      )
-    },
-    {
-      path: '/login',
-      element: (
-        <>
-          <RegisterLayout>
-            <Login />
-          </RegisterLayout>
-        </>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
       )
     }
   ])
