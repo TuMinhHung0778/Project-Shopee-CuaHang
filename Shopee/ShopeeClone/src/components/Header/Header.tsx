fac5f2412bf5ed873bbe93bfbd7acbca3a22c0a4
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-07-31 23:46:15 +0700

    190. Code chức năng tìm kiếm sản phẩm

--- Shopee/ShopeeClone/src/components/Header/Header.tsx
+++ Shopee/ShopeeClone/src/components/Header/Header.tsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom'
-import Popover from '../Popover'
-import authApi from 'src/apis/auth.api'
 import { useMutation } from '@tanstack/react-query'
-import { AppContext } from 'src/contexts/app.context'
 import { useContext } from 'react'
+import { createSearchParams, Link, useNavigate } from 'react-router-dom'
+import authApi from 'src/apis/auth.api'
 import path from 'src/constants/path'
+import { AppContext } from 'src/contexts/app.context'
+import useQueryConfig from 'src/hooks/useQueryConfig'
+import Popover from '../Popover'
+import { useForm } from 'react-hook-form'
+import { schema } from 'src/utils/rules'
+import { Schema } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { omit } from 'lodash'
+
+type FormData = Pick<Schema, 'name'>
+
+const nameSchema = schema.pick(['name'])
 
 export default function Header() {
+  const queryConfig = useQueryConfig()
+  const { register, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      name: ''
+    },
+    resolver: yupResolver(nameSchema)
+  })
+  const navigate = useNavigate()
   const { setIsAuthenticated, isAuthenticated, setProfile, profile } = useContext(AppContext)
   const logoutMutation = useMutation({
     mutationFn: authApi.logout,
@@ -19,18 +37,38 @@ export default function Header() {
   const handleLogout = () => {
     logoutMutation.mutate()
   }
+
+  const onSubmitSearch = handleSubmit((data) => {
+    const config = queryConfig.order
+      ? omit(
+          {
+            ...queryConfig,
+            name: data.name
+          },
+          ['order', 'sort_by']
+        )
+      : {
+          ...queryConfig,
+          name: data.name
+        }
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(config).toString()
+    })
+  })
+
   return (
-    <div className='pb-5 pt-2 bg-[linear-gradient(-180deg,_#f53d2d,_#f63)] text-white'>
+    <div className='bg-[linear-gradient(-180deg,_#f53d2d,_#f63)] pb-5 pt-2 text-white'>
       <div className='container'>
         {/* Language and User Profile */}
         <div className='flex justify-end'>
           <Popover
-            className='flex items-center py-1 hover:text-white/70 cursor-pointer'
+            className='flex cursor-pointer items-center py-1 hover:text-white/70'
             renderPopover={
-              <div className='bg-white relative shadow-md rounded-sm border border-gray-200'>
-                <div className='flex flex-col py-2 pr-28 pl-3'>
-                  <button className='py-2 px-3 hover:text-orange'>Tiếng Việt</button>
-                  <button className='py-2 px-3 hover:text-orange mt-2'>English</button>
+              <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
+                <div className='flex flex-col py-2 pl-3 pr-28'>
+                  <button className='px-3 py-2 hover:text-orange'>Tiếng Việt</button>
+                  <button className='mt-2 px-3 py-2 hover:text-orange'>English</button>
                 </div>
               </div>
             }
@@ -41,7 +79,7 @@ export default function Header() {
               viewBox='0 0 24 24'
               strokeWidth={1.5}
               stroke='currentColor'
-              className='w-5 h-5'
+              className='h-5 w-5'
             >
               <path
                 strokeLinecap='round'
@@ -56,42 +94,42 @@ export default function Header() {
               viewBox='0 0 24 24'
               strokeWidth={1.5}
               stroke='currentColor'
-              className='w-5 h-5'
+              className='h-5 w-5'
             >
               <path strokeLinecap='round' strokeLinejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' />
             </svg>
           </Popover>
           {isAuthenticated && (
             <Popover
-              className='flex items-center py-1 hover:text-white/70 cursor-pointer ml-6'
+              className='ml-6 flex cursor-pointer items-center py-1 hover:text-white/70'
               renderPopover={
-                <div className='bg-white relative shadow-md rounded-sm border border-gray-200'>
+                <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
                   <Link
                     to={path.profile}
-                    className='block py-2 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left'
+                    className='block w-full bg-white px-4 py-2 text-left hover:bg-slate-100 hover:text-cyan-500'
                   >
                     Tài khoản của tôi
                   </Link>
                   <Link
                     to='/'
-                    className='block py-2 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left'
+                    className='block w-full bg-white px-4 py-2 text-left hover:bg-slate-100 hover:text-cyan-500'
                   >
                     Đơn mua
                   </Link>
                   <button
                     onClick={handleLogout}
-                    className='block py-2 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left'
+                    className='block w-full bg-white px-4 py-2 text-left hover:bg-slate-100 hover:text-cyan-500'
                   >
                     Đăng xuất
                   </button>
                 </div>
               }
             >
-              <div className='w-6 h-6 mr-2 flex-shrink-0'>
+              <div className='mr-2 h-6 w-6 flex-shrink-0'>
                 <img
                   src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
                   alt='avatar'
-                  className='w-full h-full object-cover rounded-full'
+                  className='h-full w-full rounded-full object-cover'
                 />
               </div>
               <div>{profile?.email}</div>
@@ -102,7 +140,7 @@ export default function Header() {
               <Link to={path.register} className='mx-3 capitalize hover:text-white/70'>
                 Đăng ký
               </Link>
-              <div className='border-r-[1px] border-r-white/40 h-4' />
+              <div className='h-4 border-r-[1px] border-r-white/40' />
               <Link to={path.login} className='mx-3 capitalize hover:text-white/70'>
                 Đăng nhập
               </Link>
@@ -110,7 +148,7 @@ export default function Header() {
           )}
         </div>
         {/* Logo and Search Bar */}
-        <div className='grid grid-cols-12 gap-4 mt-4 items-end'>
+        <div className='mt-4 grid grid-cols-12 items-end gap-4'>
           <Link to='/' className='col-span-2'>
             <svg viewBox='0 0 192 65' className='h-11 w-full fill-white'>
               <g fillRule='evenodd'>
@@ -120,16 +158,16 @@ export default function Header() {
           </Link>
 
           {/* Search bar */}
-          <form className='col-span-9'>
-            <div className='bg-white rounded-sm p-1 flex'>
+          <form className='col-span-9' onSubmit={onSubmitSearch}>
+            <div className='flex rounded-sm bg-white p-1'>
               <input
                 type='text'
-                name='search'
-                className='text-black px-3 py-2 flex-grow border-none outline-none bg-transparent text-sm'
+                className='flex-grow border-none bg-transparent px-3 py-2 text-sm text-black outline-none'
                 placeholder='Free Ship Đơn Từ 0Đ'
+                {...register('name')}
               />
               <button
-                className='rounded-sm py-2 px-6 flex-shrink-0 bg-[#fb5533] hover:opacity-90 flex items-center justify-center'
+                className='flex flex-shrink-0 items-center justify-center rounded-sm bg-[#fb5533] px-6 py-2 hover:opacity-90'
                 type='submit'
               >
                 <svg
@@ -138,7 +176,7 @@ export default function Header() {
                   viewBox='0 0 24 24'
                   strokeWidth={2}
                   stroke='currentColor'
-                  className='w-5 h-5 text-white'
+                  className='h-5 w-5 text-white'
                 >
                   <path
                     strokeLinecap='round'
@@ -153,19 +191,19 @@ export default function Header() {
           <div className='col-span-1 justify-self-end'>
             <Popover
               renderPopover={
-                <div className='bg-white relative shadow-md rounded-sm border border-gray-200 max-w-[400px] text-sm'>
+                <div className='relative max-w-[400px] rounded-sm border border-gray-200 bg-white text-sm shadow-md'>
                   <div className='p-2'>
-                    <div className='text-gray-400 capitalize'>Sản phẩm mới thêm</div>
+                    <div className='capitalize text-gray-400'>Sản phẩm mới thêm</div>
                     <div className='mt-5'>
                       <div className='mt-4 flex'>
                         <div className='flex-shrink-0'>
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -179,10 +217,10 @@ export default function Header() {
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -196,10 +234,10 @@ export default function Header() {
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -213,10 +251,10 @@ export default function Header() {
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -230,10 +268,10 @@ export default function Header() {
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -247,10 +285,10 @@ export default function Header() {
                           <img
                             src='https://down-vn.img.susercontent.com/file/c3f3edfaa9f6dafc4825b77d8449999d'
                             alt='anh'
-                            className='w-11 h-11 object-cover'
+                            className='h-11 w-11 object-cover'
                           />
                         </div>
-                        <div className='flex-grow ml-2 overflow-hidden'>
+                        <div className='ml-2 flex-grow overflow-hidden'>
                           <div className='truncate'>
                             [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24 cm
                           </div>
@@ -260,9 +298,9 @@ export default function Header() {
                         </div>
                       </div>
                     </div>
-                    <div className='flex mt-6 items-center justify-between'>
+                    <div className='mt-6 flex items-center justify-between'>
                       <div className='captitalize text-xs text-gray-500'>Thêm vào giỏ hàng</div>
-                      <button className='capitalize bg-orange hover:bg-opacity-90 px-4 py-2 rounded-sm text-white'>
+                      <button className='rounded-sm bg-orange px-4 py-2 capitalize text-white hover:bg-opacity-90'>
                         Xem giỏ hàng
                       </button>
                     </div>
@@ -277,7 +315,7 @@ export default function Header() {
                   viewBox='0 0 24 24'
                   strokeWidth={1.5}
                   stroke='currentColor'
-                  className='w-8 h-8'
+                  className='h-8 w-8'
                 >
                   <path
                     strokeLinecap='round'
--- Shopee/ShopeeClone/src/pages/ProductList/ProductList.tsx
+++ Shopee/ShopeeClone/src/pages/ProductList/ProductList.tsx
@@ -1,6 +1,4 @@
 import { useQuery } from '@tanstack/react-query'
-import { omitBy, isUndefined } from 'lodash'
-import useQueryParam from 'src/hooks/useQueryParams'
 import productApi from 'src/apis/product.api'
 import Pagination from 'src/components/Pagination'
 import categoryApi from 'src/apis/category.api'
@@ -8,28 +6,11 @@ import type { ProductListConfig } from 'src/types/product.type'
 import AsideFilter from './components/AsideFilter'
 import Product from './components//Product/Product'
 import SortProductList from './components/SortProductList'
-
-export type QueryConfig = {
-  [key in keyof ProductListConfig]: string
-}
+import useQueryConfig from 'src/hooks/useQueryConfig'
 
 export default function ProductList() {
-  const queryParams: QueryConfig = useQueryParam()
-  const queryConfig: QueryConfig = omitBy(
-    {
-      page: queryParams.page || '1',
-      limit: queryParams.limit || '20',
-      sort_by: queryParams.sort_by,
-      exclude: queryParams.exclude,
-      name: queryParams.name,
-      order: queryParams.order,
-      price_max: queryParams.price_max,
-      price_min: queryParams.price_min,
-      rating_filter: queryParams.rating_filter,
-      category: queryParams.category
-    },
-    isUndefined
-  )
+  const queryConfig = useQueryConfig()
+
   const { data: productsData } = useQuery({
     queryKey: ['products', queryConfig],
     queryFn: () => {
